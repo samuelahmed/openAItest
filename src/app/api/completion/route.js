9b8d27b82ba913491d637d7f543b33a94f152f0e
samuelahmed <samueladuval@gmail.com>
2023-12-06 18:32:18 -0800

    add error handling

--- src/app/api/completion/route.js
+++ src/app/api/completion/route.js
@@ -5,14 +5,20 @@ const openai = new OpenAI({
 });
 
 export async function POST(request) {
+  try {
+    const { content } = await request.json();
 
-  const { content } = await request.json();
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: "system", content: content }],
+      model: "gpt-3.5-turbo",
+    });
 
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: content }],
-    model: "gpt-3.5-turbo",
-  });
-
-  console.log("Completion Called");
-  return new Response(JSON.stringify(completion), { status: 200 });
+    console.log("Completion Called");
+    return new Response(JSON.stringify(completion), { status: 200 });
+  } catch (error) {
+    console.error("Error:", error);
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
 }
