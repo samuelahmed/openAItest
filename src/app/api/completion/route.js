68eb2891476805abb2869bc62f9a89298d0d0e24
samuelahmed <samueladuval@gmail.com>
2023-12-06 18:56:49 -0800

    add timeout

--- src/app/api/completion/route.js
+++ src/app/api/completion/route.js
@@ -4,17 +4,25 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-export const runtime = 'edge'; 
+export const runtime = "edge";
 
+function withTimeout(promise, ms) {
+  const timeout = new Promise((_, reject) =>
+    setTimeout(() => reject(new Error("Request timed out")), ms)
+  );
+  return Promise.race([promise, timeout]);
+}
 
 export async function POST(request) {
   try {
     const { content } = await request.json();
 
-    const completion = await openai.chat.completions.create({
+    const completionPromise = await openai.chat.completions.create({
       messages: [{ role: "system", content: content }],
       model: "gpt-3.5-turbo",
     });
+
+    const completion = await withTimeout(completionPromise, 24000);
 
     console.log("Completion Called");
     return new Response(JSON.stringify(completion), { status: 200 });
